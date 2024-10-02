7230e59c3a35e2861513b674914a7ba35703c82b
SantellaA <codicegamer@gmail.com>
2024-10-02 17:36:44 -0300

    agrego los metodos a el user controller

--- laravel-api/app/Http/Controllers/Api/userController.php
+++ laravel-api/app/Http/Controllers/Api/userController.php
@@ -3,9 +3,162 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
+use Exception;
+
+use App\Models\User;
 
 class userController extends Controller
 {
-    //
+    public function list(){
+        $response =[];
+        
+        try{
+            $usuarios = User::all(); 
+
+            if($usuarios->isEmpty()){
+                $response['message'] = 'No hay usuarios cargados en la base de datos';
+            }else{
+                $response['message'] = 'Listado de usuarios';
+            }
+
+            $response['data'] = $usuarios;    
+            $response['status'] = 200;
+
+        }catch(Exception $e){
+            $response = [
+                'message' => 'Error, Ocurrio un error obteniendo el istado de usuarios',
+                'errors' => $e,
+                'status' => 500
+            ];
+        }
+        
+        return response()->json($response, $response['status']);
+    }
+
+    public function get(int $id){
+        $response =[];
+        
+        try{
+            $instance = User::findOrFail($id);
+
+            $response = [
+                'message' => $instance,
+                'status' => 200
+            ];
+
+        }catch(Exception $e){
+            $response = [
+                'message' => 'Error, Ocurrio un error obteniendo el usuario con el id: ' . $id,
+                'errors' => $e,
+                'status' => 500
+            ];
+        }
+
+        return response()->json($response, $response['status']);
+    }
+
+    public function create(Request $request){
+        $response =[];
+        
+        try{
+            $validator = Validator::make($request->all(), [
+                'nombre'=> 'required',
+                'piso' => 'required'
+            ]);
+
+            if($validator->fails()){
+                $response =[
+                    'message' => 'Error, Datos invalidos',
+                    'errors' => $validator->errors(),
+                    'status' => 400 
+                ];
+            }else{
+                $usuario = User::create([
+                    'nombre'=> $request->nombre,
+                    'piso' => $request->piso
+                ]);
+
+                $response = [
+                    'message' => 'Se creo correctamente un nuevo usuario',
+                    'data' => $usuario,
+                    'status' => 201
+                ];
+            }
+        }catch(Exception $e){
+            $response = [
+                'message' => 'Error, Ocurrio un error creando un nuevo usuario',
+                'errors' => $e,
+                'status' => 500
+            ];
+        }
+
+        return response()->json($response, $response['status']); 
+    }
+
+    public function update(Request $request, int $id){
+        $response =[];
+
+        try{
+            $usuario = User::findOrfail($id);
+
+            $validator = Validator::make($request->all(), [
+                'nombre'=> 'required',
+                'piso' => 'required'
+            ]);
+
+            if($validator->fails()){
+                $response =[
+                    'message' => 'Error, Datos invalidos',
+                    'errors' => $validator->errors(),
+                    'status' => 400 
+                ];
+            }else{
+                $usuario->update($validator->validated());
+
+                $response = [
+                    'message' => 'se acualizo correctamente el usuario con el id: ' . $id,
+                    'data' => $usuario,
+                    'status' => 200
+                ];
+            }
+        }catch(Exception $e){
+            $response = [
+                'message' => 'Error, Ocurrio un error actualizando el usuario con el id: ' . $id,
+                'errors' => $e,
+                'status' => 500
+            ];
+        }
+
+        return response()->json($response, $response['status']);
+    }
+
+    public function delete(int $id){
+        $response =[];
+
+        try{
+            $instance = User::findOrFail($id);
+
+            /*if (!auth()->user()->can('delete', $instance)) {
+                return response()->json(['message' => 'No tienes permisos para eliminar este usuario.'], 403);
+            }*/
+        
+            $instance->delete();
+
+            $response = [
+                'message' => 'usuario eliminado exitosamente',
+                'status' => 200
+            ];
+
+        }catch(Exception $e){
+            $response = [
+                'message' => 'Error, Ocurrio un error eliminando el usuario con el id: ' . $id,
+                'errors' => $e,
+                'status' => 500
+            ];
+        }
+
+        return response()->json($response, $response['status']); 
+    }
 }
