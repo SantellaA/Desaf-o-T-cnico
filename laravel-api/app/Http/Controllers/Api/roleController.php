2ebb712005abbd6e1a105ea87bd1459514ea587f
SantellaA <codicegamer@gmail.com>
2024-10-03 14:07:16 -0300

    implemento el apiTrait en el RoleController

--- laravel-api/app/Http/Controllers/Api/roleController.php
+++ laravel-api/app/Http/Controllers/Api/roleController.php
@@ -6,15 +6,19 @@ use App\Http\Controllers\Controller;
 use Exception;
 
 use App\Models\Role;
-
+use App\Traits\ApiTrait;
 
 class roleController extends Controller
 {
+    use ApiTrait;
+
     public function list(){
         $response =[];
         
         try{
             $roles = Role::all(); 
+
+            $response =  $this->formatearRespuesta('', 200, $roles);
 
             if($roles->isEmpty()){
                 $response['message'] = 'No hay roles cargados en la base de datos';
@@ -22,15 +26,8 @@ class roleController extends Controller
                 $response['message'] = 'Listado de roles';
             }
 
-            $response['data'] = $roles;    
-            $response['status'] = 200;
-
         }catch(Exception $e){
-            $response = [
-                'message' => 'Error, Ocurrio un error obteniendo el istado de roles',
-                'errors' => $e,
-                'status' => 500
-            ];
+            $response =  $this->formatearRespuesta('Error, Ocurrio un error obteniendo el istado de roles', 500, $e, true);
         }
         
         return response()->json($response, $response['status']);
@@ -42,17 +39,10 @@ class roleController extends Controller
         try{
             $instance = Role::findOrFail($id);
 
-            $response = [
-                'message' => $instance,
-                'status' => 200
-            ];
+            $response =  $this->formatearRespuesta('GET de Role con id: ' .$id, 200, $instance);
 
-        }catch(Exception $e){
-            $response = [
-                'message' => 'Error, Ocurrio un error obteniendo el role con el id: ' . $id,
-                'errors' => $e,
-                'status' => 500
-            ];
+        }catch(Exception $e){;
+            $response =  $this->formatearRespuesta('Error, Ocurrio un error obteniendo el role con el id: ' . $id, 500, $e, true);
         }
 
         return response()->json($response, $response['status']);
